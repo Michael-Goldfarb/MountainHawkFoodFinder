2a44c5155b1652073fcad830b3d2b05274c44baf
mlg325 <mlg325@lehigh.edu>
2023-06-24 20:49:02 -0400

    Initial Commit

---
+++ LehighSustainability/LehighSustainabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  LehighSustainabilityApp.swift
+//  LehighSustainability
+//
+//  Created by Michael Goldfarb on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LehighSustainabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
