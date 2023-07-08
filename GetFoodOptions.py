16ec8acf44bbdee7dbedd38b4e0af11e5216188c
mlg325 <mlg325@lehigh.edu>
2023-07-08 18:12:43 -0400

    Able to print the daily food options, but can't get calories

---
+++ GetFoodOptions.py
@@ -0,0 +1,82 @@
+import psycopg2
+import requests
+import re
+from bs4 import BeautifulSoup
+
+# # Set up connection to ElephantSQL
+# conn = psycopg2.connect(
+#     host = 'rajje.db.elephantsql.com',
+#     database = 'syabkhtb',
+#     user = 'syabkhtb',
+#     port = '5432',
+#     password = 'J7LXI5pNQ_UoUP316yEd-yoXnCOZK8HE'
+# )
+
+# URL of the menu page
+url = "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=16872&locationId=97451005&whereami=http://lehigh.sodexomyway.com/dining-near-me/rathbone"
+
+# Send a GET request to retrieve the HTML content of the page
+response = requests.get(url)
+
+print(response)
+
+# Create a BeautifulSoup object to parse the HTML content
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find the menu items within the specific div
+menu_div = soup.find('div', id='menuid-2-day')
+
+# Find the bite-menu-course sections
+course_sections = menu_div.find_all('div', class_='bite-menu-course')
+
+# Iterate over the course sections and extract menu items with calories
+for course_section in course_sections:
+    # Extract the course name
+    course_name = course_section.find('h5').text
+    print(f"Course: {course_name}")
+
+    # Extract the menu items within the course section
+    menu_items = course_section.find_next('ul', class_='bite-menu-item').find_all('a', class_='get-nutritioncalculator')
+
+    # Extract the menu item names and calorie amounts
+    for item in menu_items:
+        menu_item_name = item['data-fooditemname']
+        calorie_text = item.text.strip()
+        # Extract the calorie amount if a match is found, otherwise assign 0 as the default value
+        match = re.search(r'\d+', calorie_text)
+        calorie_amount = match.group() if match else '0'
+        print(f"{menu_item_name}: {calorie_amount} calories")
+
+
+    print()  # Add a blank line to separate courses
+
+
+
+
+# cursor = conn.cursor()
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS rathboneOptions (
+        
+#     )
+# """)
+# conn.commit()
+
+
+
+
+
+
+
+
+# # Insert data into the table
+# cursor.execute("TRUNCATE TABLE rathboneOptions;")
+
+# cursor.executemany("""
+#     INSERT INTO rathboneOptions ()
+#     VALUES ()
+# """, )
+
+# # Commit the changes and close the cursor and connection
+# conn.commit()
+# cursor.close()
+# conn.close()
