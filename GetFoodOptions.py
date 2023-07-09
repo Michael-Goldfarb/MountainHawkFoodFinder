d5badd8e02c5197478631bb0c7015f4caca09dd4
mlg325 <mlg325@lehigh.edu>
2023-07-09 04:11:06 -0400

    tried to get current menu but menu is down right now

--- GetFoodOptions.py
+++ GetFoodOptions.py
@@ -23,20 +23,12 @@ print(response)
 # Create a BeautifulSoup object to parse the HTML content
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Find the menu items within the specific div
-menu_div = soup.find('div', id='menuid-2-day')
+# Find the bite-menu-dates section
+menu_dates = soup.find('ul', class_='primary-backgroundcolor')
 
-# Find the bite-menu-course sections
-course_sections = menu_div.find_all('div', class_='bite-menu-course')
-
-# Iterate over the course sections and extract menu items with calories
-for course_section in course_sections:
-    # Extract the course name
-    course_name = course_section.find('h5').text
-    print(f"Course: {course_name}")
-
-    # Extract the menu items within the course section
-    menu_items = course_section.find_next('ul', class_='bite-menu-item').find_all('a', class_='get-nutritioncalculator')
+try:
+    # Find the menu items within the current-menu date
+    menu_items = menu_dates.find('li', class_='bite-date current-menu').find_next('ul', class_='bite-menu-item').find_all('a', class_='get-nutritioncalculator')
 
     # Extract the menu item names and calorie amounts
     for item in menu_items:
@@ -45,8 +37,8 @@ for course_section in course_sections:
         calorie_text = calorie_element.text.strip() if calorie_element else '0'
         print(f"{menu_item_name}: {calorie_text} calories")
 
-
-    print()  # Add a blank line to separate courses
+except AttributeError:
+    print("No menu found")
 
 
 
