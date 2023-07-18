5eeb5ff031554c7a3a16929d2f861f053f66eb8e
mlg325 <mlg325@lehigh.edu>
2023-07-18 00:44:49 -0400

    added all of eatery places to the map

--- mobile/LehighFoodFinder/HomeViewModel.swift
+++ mobile/LehighFoodFinder/HomeViewModel.swift
@@ -10,8 +10,50 @@ import MapKit
 
 class NavigationState: ObservableObject {
     @Published var isButtonClicked = false
+    @Published var selectedLocation = ""
     
     func navigateToRathboneDetails() {
+        selectedLocation = "Rathbone Dining Hall"
+        isButtonClicked = true
+    }
+    
+    func navigateToHawksNestOptions() {
+        selectedLocation = "Hawk's Nest"
+        isButtonClicked = true
+    }
+    
+    func navigateToTheGrindOptions() {
+        selectedLocation = "The Grind @ FML"
+        isButtonClicked = true
+    }
+    
+    func navigateToCommonGroundsOptions() {
+        selectedLocation = "Common Grounds"
+        isButtonClicked = true
+    }
+    
+    func navigateToHideawayCafeOptions() {
+        selectedLocation = "The Hideaway Cafe"
+        isButtonClicked = true
+    }
+    
+    func navigateToLucysCafeOptions() {
+        selectedLocation = "Lucy's Café"
+        isButtonClicked = true
+    }
+    
+    func navigateToHillsideCafeOptions() {
+        selectedLocation = "Hillside Cafe"
+        isButtonClicked = true
+    }
+    
+    func navigateToGlobalCafeOptions() {
+        selectedLocation = "Global Cafe"
+        isButtonClicked = true
+    }
+    
+    func navigateToIacoccaCafeOptions() {
+        selectedLocation = "Iacocca Café"
         isButtonClicked = true
     }
 }
@@ -32,6 +74,30 @@ struct MapBackgroundView: UIViewRepresentable {
         // Add annotations
         let rathboneAnnotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: 40.606703, longitude: -75.372916), title: "Rathbone Dining Hall")
         mapView.addAnnotation(rathboneAnnotation)
+        
+        let hawksNestAnnotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: 40.605568, longitude: -75.376236), title: "Hawk's Nest")
+        mapView.addAnnotation(hawksNestAnnotation)
+        
+        let grindAnnotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: 40.609062, longitude: -75.378034), title: "The Grind @ FML")
+        mapView.addAnnotation(grindAnnotation)
+        
+        let commonGroundsAnnotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: 40.608202, longitude: -75.373936), title: "Common Grounds")
+        mapView.addAnnotation(commonGroundsAnnotation)
+        
+        let hideawayCafeAnnotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: 40.60946, longitude: -75.37688), title: "The Hideaway Cafe")
+        mapView.addAnnotation(hideawayCafeAnnotation)
+        
+        let lucysCafeAnnotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: 40.606701, longitude: -75.376968), title: "Lucy's Café")
+        mapView.addAnnotation(lucysCafeAnnotation)
+        
+        let hillsideCafeAnnotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: 40.60456, longitude: -75.3797), title: "Hillside Cafe")
+        mapView.addAnnotation(hillsideCafeAnnotation)
+        
+        let globalCafeAnnotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: 40.606739, longitude: -75.37563), title: "Global Cafe")
+        mapView.addAnnotation(globalCafeAnnotation)
+        
+        let iacoccaCafeAnnotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: 40.601456, longitude: -75.359968), title: "Iacocca Café")
+        mapView.addAnnotation(iacoccaCafeAnnotation)
         
         return mapView
     }
@@ -75,6 +141,22 @@ struct MapBackgroundView: UIViewRepresentable {
             if let annotation = view.annotation as? CustomAnnotation {
                 if annotation.title == "Rathbone Dining Hall" {
                     mapBackgroundView.navigationState.navigateToRathboneDetails()
+                } else if annotation.title == "Hawk's Nest" {
+                    mapBackgroundView.navigationState.navigateToHawksNestOptions()
+                } else if annotation.title == "The Grind @ FML" {
+                    mapBackgroundView.navigationState.navigateToTheGrindOptions()
+                } else if annotation.title == "Common Grounds" {
+                    mapBackgroundView.navigationState.navigateToCommonGroundsOptions()
+                } else if annotation.title == "The Hideaway Cafe" {
+                    mapBackgroundView.navigationState.navigateToHideawayCafeOptions()
+                } else if annotation.title == "Lucy's Café" {
+                    mapBackgroundView.navigationState.navigateToLucysCafeOptions()
+                } else if annotation.title == "Hillside Cafe" {
+                    mapBackgroundView.navigationState.navigateToHillsideCafeOptions()
+                } else if annotation.title == "Global Cafe" {
+                    mapBackgroundView.navigationState.navigateToGlobalCafeOptions()
+                } else if annotation.title == "Iacocca Café" {
+                    mapBackgroundView.navigationState.navigateToIacoccaCafeOptions()
                 }
             }
         }
@@ -97,9 +179,39 @@ class NavigationManager {
     private init() {}
     
     func navigateToRathboneDetails() {
-        // Perform navigation to RathboneDetailsView
-        // You can replace this with your desired navigation method
         print("Navigating to RathboneDetailsView")
+    }
+    
+    func navigateToHawksNestOptions() {
+        print("Navigating to HawksNestOptionsView")
+    }
+    
+    func navigateToTheGrindOptions() {
+        print("Navigating to TheGrindOptionsView")
+    }
+    
+    func navigateToCommonGroundsOptions() {
+        print("Navigating to CommonGroundsOptionsView")
+    }
+    
+    func navigateToHideawayCafeOptions() {
+        print("Navigating to HideawayCafeOptionsView")
+    }
+    
+    func navigateToLucysCafeOptions() {
+        print("Navigating to LucysCafeOptionsView")
+    }
+    
+    func navigateToHillsideCafeOptions() {
+        print("Navigating to HillsideCafeOptionsView")
+    }
+    
+    func navigateToGlobalCafeOptions() {
+        print("Navigating to GlobalCafeOptionsView")
+    }
+    
+    func navigateToIacoccaCafeOptions() {
+        print("Navigating to IacoccaCafeOptionsView")
     }
 }
 
@@ -109,13 +221,31 @@ struct HomeView: View {
     var body: some View {
         NavigationView {
             ZStack {
-                if !navigationState.isButtonClicked { // Only display MapBackgroundView if isButtonClicked is false
+                if !navigationState.isButtonClicked {
                     MapBackgroundView()
                         .edgesIgnoringSafeArea(.all)
                 }
                 
                 if navigationState.isButtonClicked {
-                    RathboneDetailsView()
+                    if navigationState.selectedLocation == "Rathbone Dining Hall" {
+                        RathboneDetailsView()
+                    } else if navigationState.selectedLocation == "Hawk's Nest" {
+                        HawksNestDetailsView()
+                    } else if navigationState.selectedLocation == "The Grind @ FML" {
+                        TheGrindDetailsView()
+                    } else if navigationState.selectedLocation == "Common Grounds" {
+                        CommonGroundsDetailsView()
+                    } else if navigationState.selectedLocation == "The Hideaway Cafe" {
+                        HideawayCafeDetailsView()
+                    } else if navigationState.selectedLocation == "Lucy's Café" {
+                        LucysCaféDetailsView()
+                    } else if navigationState.selectedLocation == "Hillside Cafe" {
+                        HillsideCafeDetailsView()
+                    } else if navigationState.selectedLocation == "Global Cafe" {
+                        GlobalCaféDetailsView()
+                    } else if navigationState.selectedLocation == "Iacocca Café" {
+                        IacoccaCaféDetailsView()
+                    }
                 }
             }
             .navigationBarTitle("", displayMode: .inline)
@@ -123,10 +253,9 @@ struct HomeView: View {
     }
 }
 
-
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
-            .environmentObject(NavigationState()) // Provide the NavigationState environment object
+            .environmentObject(NavigationState())
     }
 }
---
+++ mobile/LehighFoodFinder/LucysCafeDetailsView.swift
@@ -0,0 +1,57 @@
+//
+//  LucysCaféDetailsView.swift
+//  LehighFoodFinder
+//
+//  Created by Michael Goldfarb on 7/17/23.
+//
+
+import SwiftUI
+
+struct LucysCaféDetailsView: View {
+    @State private var isHomeViewPresented = false
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 3) {
+                Spacer()
+                Spacer()
+                
+                Text("Lucys Café")
+                    .font(.system(size: 24, weight: .bold))
+                
+                Text("Click on the Map to Go Back to the Map")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                Spacer()
+                
+                Button(action: {
+                    isHomeViewPresented = true
+                }) {
+                    Image("AppleMaps")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 33, height: 33)
+                }
+                .fullScreenCover(isPresented: $isHomeViewPresented) {
+                    HomeView()
+                        .environmentObject(NavigationState()) // Provide the NavigationState environment object
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 16)
+                
+            }
+            .background(Color.white)
+            .navigationBarTitle("", displayMode: .inline)
+            .padding(.top, -25)
+            .onAppear {
+            }
+        }
+    }
+}
+
+struct LucysCaféDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        LucysCaféDetailsView()
+    }
+}
