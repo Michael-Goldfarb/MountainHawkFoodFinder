a44559bfd95e244020491d01b5b311662076d595
mlg325 <mlg325@lehigh.edu>
2023-07-20 13:49:31 -0400

    cleaned up unwanted info for public repo

--- menuTables/WhatIsOpenNow.py
+++ menuTables/WhatIsOpenNow.py
@@ -1,4 +1,5 @@
 import psycopg2
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -9,13 +10,20 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
+# # Read database credentials from environment variables
+# db_host = os.environ.get('DB_HOST')
+# db_name = os.environ.get('DB_NAME')
+# db_user = os.environ.get('DB_USER')
+# db_port = os.environ.get('DB_PORT')
+# db_password = os.environ.get('DB_PASSWORD')
+
 # # Set up connection to ElephantSQL
 # conn = psycopg2.connect(
-#     host='mahmud.db.elephantsql.com',
-#     database='lengoefb',
-#     user='lengoefb',
-#     port='5432',
-#     password='nJQkL-JFEmmoXtUmDSDZMRhEkzhZlOiL'
+#     host=db_host,
+#     database=db_name,
+#     user=db_user,
+#     port=db_port,
+#     password=db_password
 # )
 
 # cursor = conn.cursor()
--- menuTables/otherDining.py
+++ menuTables/otherDining.py
@@ -1,13 +1,21 @@
 import psycopg2
 import requests
+import os
+
+# Read database credentials from environment variables
+db_host = os.environ.get('DB_HOST')
+db_name = os.environ.get('DB_NAME')
+db_user = os.environ.get('DB_USER')
+db_port = os.environ.get('DB_PORT')
+db_password = os.environ.get('DB_PASSWORD')
 
 # Set up connection to ElephantSQL
 conn = psycopg2.connect(
-    host='mahmud.db.elephantsql.com',
-    database='lengoefb',
-    user='lengoefb',
-    port='5432',
-    password='nJQkL-JFEmmoXtUmDSDZMRhEkzhZlOiL'
+    host=db_host,
+    database=db_name,
+    user=db_user,
+    port=db_port,
+    password=db_password
 )
 
 cursor = conn.cursor()
